a26b49225af89958107527f9a338d9245133b682
Bryan Vu <bryanvu@me.com>
2025-02-04 13:17:03 -0600

    Finished exercise

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 /** Common config for bookstore. */
 
-
-let DB_URI = `postgresql://`;
+let DB_URI = `postgresql://postgres:1329@localhost:5432
+`;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+  DB_URI = `${DB_URI}/books_test`;
 } else {
   DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
 }
-
 
 module.exports = { DB_URI };
--- data.sql
+++ data.sql
@@ -2,9 +2,9 @@ CREATE TABLE books (
   isbn TEXT PRIMARY KEY,
   amazon_url TEXT,
   author TEXT,
-  language TEXT, 
+  language TEXT,
   pages INTEGER,
   publisher TEXT,
-  title TEXT, 
+  title TEXT,
   year INTEGER
 );
